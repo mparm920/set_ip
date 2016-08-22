2d6caefc853718ce8d75c88846e9c09d82673689
mparm920 <mparm920@gmail.com>
2016-08-22 20:29:07 +0000

    creating list of interfaces

--- setIPs.py
+++ setIPs.py
@@ -9,13 +9,16 @@ parser.add_argument("-i", "--interface", help="Choose a network interface e.g. e
 
 args = parser.parse_args()
 
+interfaces = []
 def main():
     list_interfaces()
 
 def list_interfaces():
     for _, dir, _ in os.walk('/sys/class/net/'):
-        for interface in dir:
-            print(interface)
+        interfaces = dir
+        for index, interface in enumerate(dir):
+            print(str(index) + ') ' + interface)
+    print(interfaces)
 
 if __name__ == '__main__':
     main()
