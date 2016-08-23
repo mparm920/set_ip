7cbfcd17b3980b9aa8c4ac259d7fe72bd9a3d6a2
mparm920 <mparm920@gmail.com>
2016-08-23 19:36:49 +0000

    added functions to get subnet and ip address

--- setIPs.py
+++ setIPs.py
@@ -4,21 +4,39 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--interface", help="Choose a network interface e.g. eth0",
-                    action="store_true")
+parser.add_argument("-i", "--interface", help="Choose a network interface e.g. eth0")
 
 args = parser.parse_args()
 
-interfaces = []
+interfaces = {}
 def main():
     list_interfaces()
+    get_IP('eth0')
+    get_cidr('eth0')
+    get_subnet('24')
 
 def list_interfaces():
     for _, dir, _ in os.walk('/sys/class/net/'):
-        interfaces = dir
         for index, interface in enumerate(dir):
+            interfaces[index] = {'interface':interface }
             print(str(index) + ') ' + interface)
-    print(interfaces)
+
+def get_IP(interface):
+    ip = os.popen("ip a show dev " + interface + " | sed -rn 's/inet\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\/.*/\\1/p'").read()
+    print(str(ip).strip())
+
+def get_cidr(interface):
+    cidr = os.popen("ip a show dev " + interface + " | sed -rn 's/inet\s.*\/([0-9]{1,3}).*/\\1/p'").read()
+    print(cidr.strip())
+
+def get_subnet(cidr):
+    octect = int(cidr.strip()) // 8
+    remainder = int(cidr.strip()) % 8
+    subnet = ""
+    for i in range(octect):
+       subnet += '255.'
+    subnet += str(256 - 2 ** (8 - remainder)) + '.0'
+    print(subnet)
 
 if __name__ == '__main__':
     main()
