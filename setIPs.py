ed07c6fc8778d2e532b172170d354758edd88e5f
mparm920 <mparm920@gmail.com>
2016-08-24 19:30:49 +0000

    using python regex for easier unit testing

--- setIPs.py
+++ setIPs.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--interface", help="Choose a network interface e.g. eth0")
@@ -11,22 +12,29 @@ args = parser.parse_args()
 interfaces = {}
 def main():
     list_interfaces()
-    get_IP('wlp2s0')
-    get_cidr('wlp2s0')
-    get_subnet('19')
+    print(interfaces)
 
 def list_interfaces():
     for _, dir, _ in os.walk('/sys/class/net/'):
         for index, interface in enumerate(dir):
-            interfaces[index] = {'interface':interface }
+            interfaces[interface] = {}
+            get_IP(interface)
             print(str(index) + ') ' + interface)
 
 def get_IP(interface):
-    ip = os.popen("ip a show dev " + interface + " | sed -rn 's/inet\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\/.*/\\1/p'").read()
-    print(str(ip).strip())
+     ipSettings = os.popen('ip a show dev ' + interface).read()
+     gateway = os.popen('ip r').read()
+     get_everything(interface, ipSettings, gateway)
 
-def get_cidr(interface):
-    cidr = os.popen("ip a show dev " + interface + " | sed -rn 's/inet\s.*\/([0-9]{1,3}).*/\\1/p'").read()
+def get_everything(interface, ipSettings, gateway):
+    ipMatch = re.search('inet ([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})/', ipSettings)
+    interfaces[interface]['address'] = ipMatch.group(1)
+
+    subnetMatch = re.search(r'inet\s.*\/([0-9]{1,3}).*', ipSettings)
+    interfaces[interface]['netmask'] = get_subnet(subnetMatch.group(1))
+
+    gatewayMatch = re.search(r'default\svia\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*', gateway)
+    interfaces[interface]['gateway'] = gatewayMatch.group(1)
 
 def get_subnet(cidr):
     octect = int(cidr.strip()) // 8
