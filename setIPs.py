1c6ad000392bd8ca041ddf38c0030918fa6bd649
mparm920 <mparm920@gmail.com>
2016-08-25 19:31:10 +0000

    starting to take user input

--- setIPs.py
+++ setIPs.py
@@ -13,7 +13,19 @@ interfaces = {}
 userSelection = []
 def main():
     list_interfaces()
-    print(interfaces)
+    while True:
+        userInterface = int(input("Choose interface\n"))
+        interface = userSelection[userInterface]
+        display_interface(interface)
+        address = input('Set new IP address')
+        netmask = input('Set new subnet')
+        interfaces[interface]['address'] = address
+        interfaces[interface]['netmask'] = netmask
+        set_IP(interface)
+
+def display_interface(interface):
+    for k, v in interfaces[interface].items():
+        print(str(k) + ' : ' + str(v))
 
 def list_interfaces():
     for _, dir, _ in os.walk('/sys/class/net/'):
@@ -30,13 +42,16 @@ def get_IP(interface):
 
 def get_everything(interface, ipSettings, gateway):
     ipMatch = re.search('inet ([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})/', ipSettings)
-    interfaces[interface]['address'] = ipMatch.group(1)
+    if ipMatch is not None:
+        interfaces[interface]['address'] = ipMatch.group(1)
 
     subnetMatch = re.search(r'inet\s.*\/([0-9]{1,3}).*', ipSettings)
-    interfaces[interface]['netmask'] = get_subnet(subnetMatch.group(1))
+    if subnetMatch is not None:
+        interfaces[interface]['netmask'] = get_subnet(subnetMatch.group(1))
 
     stateMatch = re.search(r'state ([D-W]{2,7}) ', ipSettings)
-    interfaces[interface]['state'] = stateMatch.group(1)
+    if stateMatch is not None:
+        interfaces[interface]['state'] = stateMatch.group(1)
 
     gatewayMatch = re.search(r'default\svia\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*', gateway)
     interfaces[interface]['gateway'] = gatewayMatch.group(1)
@@ -52,7 +67,10 @@ def get_subnet(cidr):
     return '.'.join(subnet)
 
 def set_IP(interface):
-    pass    
+    os.popen('sudo ip addr flush dev ' + interface)
+    ip = interfaces[interface]['address']
+    ip += '/' + interfaces[interface]['netmask']
+    os.popen('sudo ip addr add ' + ip + ' dev ' + interface)
 
 if __name__ == '__main__':
     main()
